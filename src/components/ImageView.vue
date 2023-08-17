53a38fb4b7b107a70c3624cf47fd9bc21994f9d7
Nabid Kaisar <himelkaisar023@gmail.com>
2023-08-17 18:25:59 +0600

    Data randomization technique included

--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { imageList } from '@/constants/CONSTANTS'
 import question from '@/assets/question.png'
-import { ref } from 'vue'
+import { computed, ref, onMounted } from 'vue'
+import { shuffleArray } from '@/util/Util'
 
 const width = '100'
 const height = '100'
 const timeout = '2000'
-const imagesCollection = ref(
-  Array.from(Array(imageList.length).fill(), (_, i) => {
-    return { id: i, src: question }
-  })
-)
+
+let randomizedData = ref([])
+
+onMounted(() => {
+  let imagesLength = imageList.length
+
+  for (let i = 0; i < imagesLength; i++) {
+    randomizedData.value.push({ id: i, src: question })
+    randomizedData.value.push({ id: i, src: question })
+  }
+
+  shuffleArray(randomizedData.value)
+  console.log(randomizedData)
+})
 
 const handleImageClick = (e, imgData) => {
   let { src: originalSrc } = e.target
@@ -24,11 +34,11 @@ const handleImageClick = (e, imgData) => {
 
 <template>
   <section class="image-gallery">
-    <div :key="idx" v-for="(imgData, idx) in imagesCollection" class="image-container">
+    <div :key="idx" v-for="(imageData, idx) in randomizedData" class="image-container">
       <img
-        @click="(e) => handleImageClick(e, imgData)"
+        @click="(e) => handleImageClick(e, imageData)"
         :id="idx"
-        :src="imgData.src"
+        :src="imageData.src"
         :width="width"
         :height="height"
         alt="humanPortrait"
